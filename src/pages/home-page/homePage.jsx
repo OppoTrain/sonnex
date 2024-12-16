0934151526dfe2d7eb450f06620a8dd6d077e85e
rama <ramahahawash2003>
2024-12-16 12:56:45 +0200

    start of homePage

---
+++ src/pages/home-page/homePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homePage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function HomePage() {
+  return (
+    <div id="root">
+      <p>home page</p>
+    </div>
+  );
+}
+
+export default HomePage;
