cdc309e06fa7d43b836ab077677e4053bf0c889b
rama <ramahahawash2003>
2024-12-20 14:15:25 +0200

    navBar

---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import img from "../../assets/logo.png";
+import "./Navbar.css";
+import { useTranslation } from "react-i18next";
+import { Dropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom"; // استيراد useNavigate
+
+import { Link } from "react-router-dom";
+import englishFlag from "../../assets/en.png";
+import arabicFlag from "../../assets/pl.png";
+
+function NavBar() {
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate(); // استخدام useNavigate للتوجيه
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
+  // دالة لإعادة التوجيه عند النقر على اللوجو
+  const handleLogoClick = () => {
+    navigate("/"); // سيتم التوجيه إلى الصفحة الرئيسية
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Dropdown>
+            <Dropdown.Toggle variant="light" className="icon">
+              <FontAwesomeIcon icon={faGlobe} />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => changeLanguage("en")}>
+                <img src={englishFlag} alt="English" className="flag-icon" />
+                English
+              </Dropdown.Item>
+
+              <Dropdown.Item onClick={() => changeLanguage("ar")}>
+                <img src={arabicFlag} alt="Arabic" className="flag-icon" />
+                Arabic
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <div className="logo-container" onClick={handleLogoClick}>
+            <img src={img} alt="Logo" className="logo" />
+          </div>
+
+          <div className="spacer"></div>
+
+          <button className="button">{t("product")}</button>
+
+          <div className="spacer"></div>
+
+          <FontAwesomeIcon icon={faBars} className="icon" />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
--- src/pages/home-page/homePage.jsx
+++ src/pages/home-page/homePage.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./homePage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slider from "../../components/slider/slider";
-
+import NavBar from "../../components/navBar/navBar";
 function HomePage() {
   return (
     <div id="root">
+      <NavBar />
       <Slider />
     </div>
   );
