0940c5f5c629091b43567dfb78ac57c948596e8e
zainasaadeddin <zeina.sadal.deen@hotmail.com>
2025-02-02 17:53:47 +0200

    testdeploy21

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-import i18n from "i18next";
+//import i18n from "i18next";
 //import { useTranslation } from "react-i18next";
 import i18n from 'https://cdn.jsdelivr.net/npm/i18n@0.15.1/+esm'
 import reactI18next from 'https://cdn.jsdelivr.net/npm/react-i18next@15.4.0/+esm'
