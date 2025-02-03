d655e981f75954816dd83dc1a7f037ffa81d6660
zainasaadeddin <zeina.sadal.deen@hotmail.com>
2025-02-03 13:25:39 +0200

    testhappyfordeploy

--- src/pages/home-page/homePage.jsx
+++ src/pages/home-page/homePage.jsx
@@ -3,13 +3,14 @@ import "./homePage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slider from "../../components/slider/slider";
 import NavBar from "../../components/navBar/navBar";
-
+import All from "../pages/all";
 
 function HomePage() {
   return (
     <div id="root">
       <NavBar />
       <Slider />
+      <All />
     </div>
   );
 }
