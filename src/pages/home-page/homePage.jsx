aeaeb32c5d2d4664f7173b4fdea7c3e53b751c23
zainasaadeddin <zeina.sadal.deen@hotmail.com>
2025-02-03 12:16:56 +0200

    testdeploy74

--- src/pages/home-page/homePage.jsx
+++ src/pages/home-page/homePage.jsx
@@ -3,7 +3,7 @@ import "./homePage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slider from "../../components/slider/slider";
 import NavBar from "../../components/navBar/navBar";
-import "./i18n"; 
+
 
 function HomePage() {
   return (
